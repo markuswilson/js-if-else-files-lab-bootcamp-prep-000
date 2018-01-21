1d8392094b7864b4da2ace875f1ff23425d9e526
Mark Wilson <markus_wilson@msn.com>
2018-01-21 06:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var name = "Joe";
 var height = "74";
 
 if (name === 'Susan') {
-  console.log('The name is incorrect')
-} else {;
+  console.log('The name is incorrect');
+} else {
   console.log('Expected the name to be Susan');
 }
 
